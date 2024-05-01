c4d2cfa361859e359c76df79153adb7386615755
tmetcalfe89 <tmetcalfe89@gmail.com>
2024-05-01 00:34:49 -0700

    hmmdedmm

--- src/main/kotlin/com/{{authorname}}/{{sidemod}}/{{SideMod}}.kt
+++ src/main/kotlin/com/{{authorname}}/{{sidemod}}/{{SideMod}}.kt
@@ -8,7 +8,7 @@ import com.{{authorname}}.{{sidemod}}.config.{{SideMod}}Config
 
 @Mod({{SideMod}}.MOD_ID)
 object {{SideMod}} {
-    const val MOD_ID = "{{side-mod}}"
+    const val MOD_ID = "{{side_mod}}"
     private var {{sideMod}}Config: {{SideMod}}Config = {{SideMod}}Config.Builder.load()
 
     @EventBusSubscriber(bus = EventBusSubscriber.Bus.FORGE)
